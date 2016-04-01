092e735c3180ac936a14a2d0f2eac5ea9d43a14b
ByronCook <Byron.a.cook@gmail.com>
2016-04-01 18:41:02 +0200

    Upload working, Date included in file name.

--- GrabMagicDesktop/GrabMagicDesktop/Screenshot.cs
+++ GrabMagicDesktop/GrabMagicDesktop/Screenshot.cs
@@ -1,16 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+using Pranas;
 
 namespace GrabMagicDesktop
 {
     class Screenshot
     {
-        public void GrabFullScreen()
+        private string _fullPath;
+        public Image GrabFullScreen()
         {
+            var image = ScreenshotCapture.TakeScreenshot();
+            var day = Convert.ToString(DateTime.Now.Day).Replace(":", "-");
+            var hour = Convert.ToString(DateTime.Now.Hour).Replace(":", "-"); ;
+            var minute = Convert.ToString(DateTime.Now.Minute).Replace(":", "-"); ;
+            var date = day + hour + minute;
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var filename = "//" + "Screenshot" + date + ".png";
+            _fullPath = path + filename;
 
+            image.Save(path + filename);
+            return image;
+        }
+
+        public void Delete()
+        {
+            File.Delete(_fullPath);
         }
     }
 }
